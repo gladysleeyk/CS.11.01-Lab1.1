432e126ed956b04bde9791a6c11488fdf2a5d024
gladyslee <gykl98@student.cis.edu.hk>
2024-01-30 12:17:31 +0800

    Completed Lab 1

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class Dateable {
 
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your age:");
+        int age = scanner.nextInt();
+        System.out.println("Age:"+ age);
+        int ageDividedByTwo = age/2;
+        int ageFinal = ageDividedByTwo +7;
+        System.out.println(age + "-year olds should date somebody who is at least " + ageFinal + "-years old.");
     }
 }
+
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your age:");
+        int age = scanner.nextInt();
+        int ageFinal = 100 - age;
+        System.out.println(age + "-year olds should read at least " + ageFinal + " pages before giving up on a book.");
     }
 }
